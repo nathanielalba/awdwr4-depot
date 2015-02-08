99f9f25571c611f7cec3fd759350e53b1c50928a
Nathaniel Alba <nathaniel.alba@gmail.com>
2015-02-08 12:41:37 -0800

    Add cache, using Russian doll

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,8 @@ class Product < ActiveRecord::Base
 		with:      %r{\.(gif|jpg|png)\Z}i,
 		message:   'must be a url for GIF, JPG or PNG image.'
 	}
-	
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -23,7 +23,7 @@ class ProductTest < ActiveSupport::TestCase
 
 		product.price = 0
 		assert product.invalid?
-		assert_equal["must be greater than or equal to 0.01"],
+		assert_equal ["must be greater than or equal to 0.01"], 
 		  product.errors[:price]
 
 		product.price = 1
