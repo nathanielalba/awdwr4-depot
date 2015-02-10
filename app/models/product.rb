7774551ef6660f8fdedb8d8a96e5d6aa1431c71d
Nathaniel Alba <nathaniel.alba@gmail.com>
2015-02-09 17:26:51 -0800

    Finished chapter

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,22 @@ class Product < ActiveRecord::Base
 		message:   'must be a url for GIF, JPG or PNG image.'
 	}
 
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	def self.latest
 		Product.order(:updated_at).last
 	end
+
+	private
+
+	  def ensure_not_referenced_by_any_line_item
+	  	if line_items.empty?
+	  		return true
+	  	else
+	  		errors.add(:base, 'Line Items present')
+	  		return false
+	  	end
+	  end
 end
